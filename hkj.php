f4adb7d682952177f232f9ee3c1ebef9af003df0
kangvcar <kangvcar@126.com>
2017-03-10 23:40:22 +0800

    add hkj.php

---
+++ hkj.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>php</title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+<form action="dohkj.php" name="f1" method="post">
+	请输入整数：<input type="text" name="n1"><br>
+    请输入数字：<input type="text" name="n2">
+    <input type="submit" name="submit" value="提交">
+</form>
+</body>
+</html>
